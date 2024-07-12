e619ec0817c0d43937feecab0ecda5cab12dd757
Muhammed Rafi <rafikandathuvayal@gmail.com>
2024-07-12 14:15:50 +0530

    ready for rehosting

--- config/dbConnecetion.js
+++ config/dbConnecetion.js
@@ -1,7 +1,19 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
 
-mongoose.connect("mongodb://127.0.0.1:27017/Z4_SHEET")
-  
+const user = process.env.MONGO_USERNAME;
+const pass = process.env.MONGO_PASSWORD;
+
+// mongoose.connect("mongodb://127.0.0.1:27017/Z4_SHEET", {
+//   auth: {
+//     username: user,
+//     password: pass,
+//   },
+//   authSource: "admin",
+// })
+
+
+mongoose.connect('mongodb+srv://rafikandathuvayal:atlasPassword@cluster0.trekfmx.mongodb.net/')
+
   .then(() => {
     console.log("DATABASE CONNECTED SUCCESFULLY");
   })
@@ -9,5 +21,5 @@ mongoose.connect("mongodb://127.0.0.1:27017/Z4_SHEET")
     console.log("FAILED TO CONNECT DATABASE");
   });
 
-  
-  module.exports=mongoose
+
+module.exports = mongoose
